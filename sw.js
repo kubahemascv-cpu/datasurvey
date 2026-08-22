24c743c3e506c263de5aac46eb1b3b09b1ddebf7
kubahemascv-cpu <kubahemascv@gmail.com>
2026-08-22 11:33:27 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // File statis, permanen di repo. Bikin Android bisa mengenali app ini
 // sebagai PWA yang bisa diinstall permanen (bukan sekadar shortcut).
 
-const CACHE = 'cs-form-v4';
+const CACHE = 'cs-form-v5';
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
